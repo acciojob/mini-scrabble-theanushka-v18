93310431995788922a16c78e7af0a160677a9c6b
Anushka Verma <93106932+theanushka-v18@users.noreply.github.com>
2023-01-06 19:51:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let input = document.querySelector("#input");
+let h3 = document.querySelector("#heading");
+let lcount = 0;
+
+function count() {
+    lcount++;
+    h3.innerText = lcount;
+    if(input.value=="") {
+        h3.innerText = 0;
+    }
+}
+
+function back() {
+    input.addEventListener('keydown', function(event) {
+        const key = event.key;
+        if (key === "Backspace") {
+            lcount = input.value.length-2;
+        }
+    });
+}
